1b934bc6658f1e08e978a41782ff87ecc80d64e8
siyamthandagwamanda <gwamandasiyamthanda13@gmail.com>
2026-08-19 10:41:23 +0200

    define types, redux toolkit is Typescript-first, before a slice can say this holds a list of shoppinglist objects, typescript needs to know what a ShoppingList is

--- src/app/types.ts
+++ src/app/types.ts
@@ -5,3 +5,30 @@ export interface User{
     surname: string;
     cellnumber: string;
 }
+
+export interface KeptUser extends User{
+    password: string;
+}
+
+export type SortKey = 'name' | 'category' | 'dataAdded'
+
+export interface ShoppingItem{
+    id: number;
+    listId: string;
+    name: string;
+    quantity: number;
+    notes: string;
+    category: string;
+    image: string;
+    checked: boolean;
+    createdAt: string;
+}
+
+export interface ShoppingList{
+    id: number;
+    userId: string;
+    name: string;
+    category: string;
+    createdAt: string;
+    updatedAt: string;
+}
